b1f7211cdf198d2afaee3acfb6b3a52cbfd047df
Vitali.Ustsilouski <v.ustilovsky@itransition.com>
2024-01-30 02:04:14 +0300

    feat: add fs functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { cp, access, constants } from "node:fs/promises";
+
+const getPath = (url, ...path) =>
+    join(dirname(fileURLToPath(url)), ...path);
+
+const isTrue = async (path) => {
+    try {
+        await access(path, constants.F_OK);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
 const copy = async () => {
-    // Write your code here 
+    const FILE_ORIGINAL = getPath(import.meta.url, "files");
+    const FILE_COPY = getPath(import.meta.url, "files_copy");
+
+    if (await isTrue(FILE_ORIGINAL) && await isTrue(FILE_COPY)) {
+        throw  new Error('FS operation file');
+    }
+
+    try{
+        await cp('src/fs/files', 'src/fs/files_copy', { recursive: true })
+
+    } catch (err) {
+        console.log(err)
+    }
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,46 @@
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { rename as fsRename } from "node:fs/promises";
+import { access, constants } from 'node:fs/promises';
+
+const getPath = (url, ...path) =>
+    join(dirname(fileURLToPath(url)), ...path);
+
+const fileTXT = getPath(
+    import.meta.url,
+    "files",
+    "wrongFilename.txt"
+);
+const fileMD = getPath(
+    import.meta.url,
+    "files",
+    "properFilename.md"
+);
+
+const isTrue = async (path) => {
+    try {
+        await access(path, constants.R_OK | constants.W_OK);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 const rename = async () => {
-    // Write your code here 
+
+    // Write your code here
+    const isTrueFileTXT = await isTrue(fileTXT);
+    const isTrueFileMD = await isTrue(fileMD);
+
+    try {
+        if (isTrueFileTXT && !isTrueFileMD) {
+            await fsRename(fileTXT, fileMD);
+        } else {
+            throw new Error('FS operation failed');
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await rename();
