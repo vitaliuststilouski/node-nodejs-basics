d93b27339231f7ac44a4141bc1b63265c2282722
Vitali.Ustsilouski <v.ustilovsky@itransition.com>
2024-01-30 02:27:24 +0300

    feat: add streams functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const getPath = (url, ...path) =>
+    join(dirname(fileURLToPath(url)), ...path);
+
+const getFilePath = getPath(import.meta.url, "files", "fileToRead.txt");
+
+const read = async (file) => {
+    const createdStream = createReadStream(file);
+    createdStream.on("data", (chunk) => process.stdout.write(chunk));
 };
 
-await read();
+await read(getFilePath);
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const getPath = (url, ...path) =>
+    join(dirname(fileURLToPath(url)), ...path);
+
+const getFilePath = getPath(import.meta.url, "files", "fileToWrite.txt");
+
+const write = async (fileType) => {
+    const streamOrigin = createWriteStream(fileType);
+    process.stdin.on("data", (chunk) => streamOrigin.write(chunk));
 };
 
-await write();
+await write(getFilePath);
